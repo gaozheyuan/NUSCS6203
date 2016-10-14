8566193cb4b4588ffaa7d481f1298dac8fca8b2d
jeffreygao13@gmail.com <jeffreygao13@gmail.com>
2016-10-14 21:07:11 +0800

    Project initialized!

---
+++ Hadoop_2_7_1/src/main/java/testhadoop/Hadoop_2_7_1/App.java
@@ -0,0 +1,13 @@
+package testhadoop.Hadoop_2_7_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
