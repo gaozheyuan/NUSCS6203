6fb7a915fbe93d1d3374b5f94fcdf96f7b501c31
jeffreygao13@gmail.com <jeffreygao13@gmail.com>
2016-11-13 17:51:35 +0800

    Graph vertex cover added

--- SubmodularFramework/src/main/java/edu/nus/submodular/graph/algorithm/impl/GraphVertexCover.java
+++ SubmodularFramework/src/main/java/edu/nus/submodular/graph/algorithm/impl/GraphVertexCover.java
@@ -1,11 +1,6 @@
 package edu.nus.submodular.graph.algorithm.impl;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.jgrapht.*;
